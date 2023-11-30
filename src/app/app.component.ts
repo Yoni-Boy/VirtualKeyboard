066257eddc92be4c86ba962032073c94b8c18de6
Nechemia Yehonatan <nechemiayoni@gmail.com>
2023-11-30 08:56:50 +0200

    Doc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { VirtualKeyboardEventsService } from 'projects/virtual-keyboard/src/public-api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'yoni-boy-libraries';
+export class AppComponent implements OnInit, AfterViewInit {
+
+  constructor(private keyboardEventsService: VirtualKeyboardEventsService) { }
+
+    ngAfterViewInit(): void {
+
+    }
+    ngOnInit(): void {
+      //Here We define the event listener, When We Focus on the Input filed  we will capture the event here
+      // Data variable is a 'VirtualKeyboardComponent' type 
+      //This event handler helps us to hide the all virtual keyboards that display and we will hide them and 
+      //Show the only specific virtual keyboard (that We focus on)
+      this.keyboardEventsService.getEvent().subscribe(data => {
+        if (data != null) {
+          const all_keyboards = document.querySelectorAll('.simple-keyboard');
+          //convert node list to an array
+          const rows = Array.from(all_keyboards) as HTMLDivElement[];
+          rows.forEach(function (value) {
+            value.hidden = true; // We hide the all keyboards component 
+          });
+          //We show the only focus input keyboard component 
+          data.div_keyboard.nativeElement.hidden = false;
+          
+        }
+        
+      });
+  }
+  public validate(value: string): boolean {
+    
+    return true;
+  }
+  public accept(value: string){
+    alert('accept:' + value);
+    
+  }
+
+  public validateNumber(value: string): boolean {
+    if (!isNaN(parseFloat(String(value))) && isFinite(Number(value)))
+      return true;
+    else
+      return false;
+  }
+
+
 }
